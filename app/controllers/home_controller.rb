d2085b3bb810475e30ad3c0e28de13f90a18f87b
Xavier Bick <fxb9500@gmail.com>
2018-04-16 23:47:52 -0500

    Adjust dates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,7 +5,7 @@ class HomeController < ApplicationController
     @pivotal_data = KeyValuePair.find_by(key: 'pivotal_data')
     @github_data = KeyValuePair.find_by(key: 'github_data')
 
-    total_days = ((Date.parse('June 6, 2018') - Date.parse('January 15, 2018')).to_int)
+    total_days = ((Date.parse('June 6, 2018') - Date.parse('January 6, 2018')).to_int)
     days_left = ((Date.parse('June 6, 2018') - Date.today).to_int)
     days_passed = total_days - days_left
     @percent_complete = ( (days_passed.to_f / total_days.to_f) * 100 ).to_i
